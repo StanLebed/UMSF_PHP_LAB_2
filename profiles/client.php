e58ef62153ef67e56b8d7858e5b32dd6f60e479d
StanLebed <61600609+StanLebed@users.noreply.github.com>
2020-06-28 13:47:54 +0300

    Update client.php

--- profiles/client.php
+++ profiles/client.php
@@ -26,28 +26,5 @@ if(!empty($_POST["select_lang"]))
 $lang = $_SESSION['lang'];
 $client = new client($_SESSION['name'], $_SESSION['surname'], $_SESSION['role'], $_SESSION['lang']);
 
-if(!empty($lang))
-{
-	if($lang == 'en')
-	{
-		$client->hello();
-	}
-	if($lang == 'ru')
-	{
-		$client->privet();
-	}
-	if($lang == 'ua')
-	{
-		$client->vitaiu();
-	}
-	if($lang == 'it')
-	{
-		$client->salve();
-	}
-}
-else
-{
-	echo "Choose the language";
-}
-require "../change_language.html"
+require "../change_language.php";
 ?>
