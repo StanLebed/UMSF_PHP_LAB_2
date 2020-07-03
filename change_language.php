5820605cc23ec5dad63e45a4d4d10ae56118f906
StanLebed <61600609+StanLebed@users.noreply.github.com>
2020-07-03 19:12:04 +0300

    Update change_language.php

--- change_language.php
+++ change_language.php
@@ -1,10 +1,22 @@
 <?php 
 $lang = $_SESSION['lang'];
 $role = $_SESSION['role'];
+
+switch($role)
+{
+	case 'admin':
+	$admin = new admin ($_SESSION['name'], $_SESSION['surname'], $_SESSION['role'], $_SESSION['lang']); break;
+	case 'manager':
+	$manager = new manager($_SESSION['name'], $_SESSION['surname'], $_SESSION['role'], $_SESSION['lang']); break;
+	case 'client':
+	$client = new client($_SESSION['name'], $_SESSION['surname'], $_SESSION['role'], $_SESSION['lang']); break;
+}
+
 if(!empty($lang))
 {
 	switch($lang) 
 	{
+
 		case 'en':	${strtolower($role)}->hello();	break;
 		case 'ru':	${strtolower($role)}->privet();	break;
 		case 'ua':	${strtolower($role)}->vitaiu();	break;
@@ -23,18 +35,18 @@ else
 </head>
 <body>
 	<div class="select_language">
-	<form method="POST">
-		<select name="lang" class="select_language_option">
-			<option value="en">English</option>
-			<option value="ru">Русский</option>
-			<option value="ua">Українська</option>
-			<option value="it">Italiano</option>
-		</select>
-		<input class="lang_btn" type="submit" name = "select_lang" value="Select">
-	</form>
+		<form method="POST">
+			<select name="lang" class="select_language_option">
+				<option value="en">English</option>
+				<option value="ru">Русский</option>
+				<option value="ua">Українська</option>
+				<option value="it">Italiano</option>
+			</select>
+			<input class="button lang_btn" type="submit" name = "select_lang" value="Select">
+		</form>
 	</div>
 	<form method="POST">
-		<a href="../logout.php"><input class = "go_back" type="submit" name="exit" value="Выйти"></a>
+		<a href="../logout.php"><input class = "button exit" type="submit" name="exit" value="Выйти"></a>
 	</form>
 </body>
 </html>  
